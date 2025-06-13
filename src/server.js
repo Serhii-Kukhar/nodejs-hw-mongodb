6e547961288fedf9fb939f6c1c51645cb54fda30
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-06-13 19:38:15 +0200

    fix bugs

--- src/server.js
+++ src/server.js
@@ -16,21 +16,22 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
-
   app.use(pino());
 
   app.get('/', (req, res) => {
     res.json({
+      status: 200,
       message: 'Hello! This is contacts.',
+      data: null,
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    console.log('GET /contacts');
+  app.get('/contacts', async (req, res, next) => {
     const contacts = await getAllContacts();
 
-    res.status(200).json({
-      message: 'Successfully found contact with id {contactId}!',
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });
@@ -40,27 +41,15 @@ export const setupServer = () => {
     const contact = await getContactById(contactId);
 
     if (!contact) {
-      res.status(404).json({
+      return res.status(404).json({
         message: 'Contact not found',
       });
-      return;
     }
 
-    res.status(200).json({
+    res.json({
+      status: 200,
+      message: 'Successfully found contact with id ${contactId}!',
       data: contact,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
     });
   });
 
