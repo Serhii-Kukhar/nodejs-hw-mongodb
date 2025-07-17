9b41e3a7d358a3170518e1170c2177067276aaec
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-07-17 21:02:24 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -29,7 +30,7 @@ export const setupServer = () => {
       data: null,
     });
   });
-
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
 
   app.use(notFoundHandler);
